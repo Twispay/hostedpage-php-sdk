24f0bbe644b7dae56839a1104fe75ff27a238096
dragos.ursu <dragos.ursu@cmdnetwork.ro>
2018-05-02 14:53:16 +0300

    fix examples config

--- examples/config.php
+++ examples/config.php
@@ -17,15 +17,4 @@ $twispayConfig = [
     ],
 ];
 
-$twispayConfig = [
-    'siteId' => 1,
-    'secretKey' => 'ea9521816d4fcf0c0a78052fa70c3df9',
-    'twispay' => [
-        'paymentUrl' => 'https://secure.twispay.lh',
-        'popupJsUrl' => 'https://secure.twispay.lh/js/sdk/v1/TwisPay.js',
-        'popupCssUrl' => 'https://secure.twispay.lh/js/sdk/v1/TwisPay.css',
-        'sslAlgorithm' => 'aes-256-cbc',
-    ],
-];
-
 return $twispayConfig;

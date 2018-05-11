69bfebb61aee87060847f986a2b3f9c6da41ca68
dragos.ursu <dragos.ursu@cmdnetwork.ro>
2018-05-11 11:58:10 +0300

    fix post back and ipn example

--- examples/config.php
+++ examples/config.php
@@ -8,7 +8,7 @@
 
 $twispayConfig = [
     'siteId' => 19246,
-    'secretKey' => '12345678901234567890abcdef123456',
+    'secretKey' => 'ea9521816d4fcf0c0a78052fa70c3df9',
     'config' => [
         'paymentUrl' => 'https://secure-stage.twispay.com',
         'popupJsUrl' => 'https://secure-stage.twispay.com/assets/script',
--- examples/postBackAndIpn.php
+++ examples/postBackAndIpn.php
@@ -22,6 +22,9 @@ $config = $twispayConfig['config'];
 // init response object
 $response = new Response($secretKey, $config);
 
+// this will come from the request (it is put here is only for example)
+$_POST['opensslResult'] = 'tqb0pHyFGeeWagIthcu/hw==,joq3TJgwFjC7UCHge4Ql7c5kyc/E42GyYfwEnEdZVOzBe5lSFptCmhDVozfffrKJhzvJ3Higsbde+Xg2agBdnOK087Vr6o/F2qEWImOwKpb3BabM9ya6tY0nRoVRx7BJQC9RIVLdTXm0nXMejBE7TM0uDO2JeTX54i2FBpg8oPd0ae82C36yz0amKNyG+7DP10EOxNjVvzQnV2npf7QdR1m2E0iGMBEO1Ykj3Fe6av3n3p15ulkym1B1NxNtVpjSdmpXcN1+ncYU4I/RGNHf6w3+bgLY/j3PCZnIfO0d7KuWQe+33WYb7a7amoYHdLYMNjhWec8GyHtyit7i82bxUV7KMTEfVXKqiaK2uC0liZBQWFY1f/K82E6gIK0Hns8ui/u/qCHUWyFehXR9T7aZQRK9q5kHQnQwPt+Aj4pK16rQF8B76hArKC8hdqZEUvTb0soJ3kpvF4+MTPq/fUCtcrgcYJhZdhGc2NJPONnfea6CjXWKhRGNB3tHmEY8z87AhPFnLBbv39uMIRMW5iOBhNxmutifcapYSZhpNo0FUWcsiQ4GkDuY6QFd7D3nkZX/';
+
 // load data from $_POST['opensslResult'] and $_GET['opensslResult'] (in this order)
 // alternatively you can pass an array containing the `opensslResult` key
 // or the encrypted response as a string value
@@ -33,11 +36,13 @@ try {
 }
 
 // do something
-echo 'Order unique identifier (externally provided): ' . $response->getExternalOrderId();
-echo 'Order ID (Twispay): ' . $response->getOrderId();
-echo 'Transaction ID (Twispay): ' . $response->getTransactionId();
-echo 'Transaction status: ' . $response->getTransactionStatus();
-echo 'Customer unique identifier (externally provided): ' . $response->getIdentifier();
-echo 'Customer ID (Twispay): ' . $response->getCustomerId();
-echo 'Amount: ' . $response->getAmount();
-echo 'Currency: ' . $response->getCurrency();
+echo '<pre>';
+echo 'Order unique identifier (externally provided): ' . $response->getExternalOrderId() . "\n";
+echo 'Order ID (Twispay ID): ' . $response->getOrderId() . "\n";
+echo 'Transaction ID (Twispay ID): ' . $response->getTransactionId() . "\n";
+echo 'Transaction status: ' . $response->getTransactionStatus() . "\n";
+echo 'Customer unique identifier (externally provided): ' . $response->getIdentifier() . "\n";
+echo 'Customer ID (Twispay ID): ' . $response->getCustomerId() . "\n";
+echo 'Amount: ' . $response->getAmount() . "\n";
+echo 'Currency: ' . $response->getCurrency() . "\n";
+echo '</pre>';
